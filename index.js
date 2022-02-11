5a567faa34cbad397bc089d40b851f84e95ecd45
Felipe Morales <felipe.morales.querol@gmail.com>
2022-02-10 22:32:31 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const { auth, requiresAuth } = require('express-openid-connect');
+
+const app = express();
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.AUTH0_SECRET,
+    baseURL: process.env.AUTH0_BASEURL,
+    clientID: process.env.AUTH0_CLIENTID,
+    issuerBaseURL: process.env.AUTH0_ISSUERBASEURL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+
+app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+});
+
+app.listen(3000, () => {
+    console.log('Server iniciado http://localhost:3000')
+});
